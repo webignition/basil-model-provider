5e2a3ca74dabc0d37fd3addd2a2639c54a2a13a9
Jon Cram <jon@webignition.net>
2019-12-02 11:53:20 +0000

    Update StepProvider

--- src/Step/EmptyStepProvider.php
+++ src/Step/EmptyStepProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace webignition\BasilModelProvider\Step;
 
-use webignition\BasilModel\Step\StepInterface;
 use webignition\BasilModelProvider\Exception\UnknownStepException;
+use webignition\BasilModels\Step\StepInterface;
 
 class EmptyStepProvider implements StepProviderInterface
 {
